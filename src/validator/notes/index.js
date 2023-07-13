f3b2eb1b3a13da1e90760097d3d37dca173b0d26
Fahmi Idris <fahmiidris@apps.ipb.ac.id>
2023-07-13 11:14:34 +0700

    First commit

---
+++ src/validator/notes/index.js
@@ -0,0 +1,13 @@
+const InvariantError = require("../../exception/InvariantError");
+const { NotePayloadsSchema } = require("./schema");
+
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResult = NotePayloadsSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = NotesValidator;
